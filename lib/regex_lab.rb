45c63c54216f08eb9da12816c4a5a2caf2d9aae2
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-29 12:00:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,8 @@ end
 
 
 def valid_phone_number?(phone)
-  if phone.match(/([0-9]){10}/)
+#  if phone.match(/([0-9]){10}/)
+   if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)   
     true 
    else 
      false 
