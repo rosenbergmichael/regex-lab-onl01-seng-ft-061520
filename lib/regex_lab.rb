97f123553c49569d7f7daa2650e8f80d264c47c2
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-29 11:39:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b\w[A-Z]       /)
+  if text.match(/\b^[A-Z]       /)
+   true 
+  else 
+    false 
+  end   
 end
 
 
